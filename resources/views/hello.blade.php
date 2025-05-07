91c2c945b4b45cb2b426d1fce376c831e4c46e5c
DFranzels <ekofranz29@gmail.com>
2025-05-07 21:22:44 +0700

    Add files via upload

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,10 @@
+<html>
+    <body>
+        <h1>hello world</h1>
+
+        <ul>
+            <li><a href="">Leonardo</a></li>
+            <li><a href="">Surbakti</a></li>
+        </ul>
+    </body>
+</html>
